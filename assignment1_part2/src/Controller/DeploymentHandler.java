e21a81c2a7ff8ce0f81ff03af0b5f3247f92b2d8
Ghost11696 <jmdillon166@gmail.com>
2017-05-21 18:38:51 +1200

    fixed deployment

--- assignment1_part2/src/Controller/DeploymentHandler.java
+++ assignment1_part2/src/Controller/DeploymentHandler.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Controller;
 
 import Model.Cell;
@@ -10,19 +5,16 @@ import Model.Grid;
 import Model.Player;
 import Model.Ship;
 import View.GameWindow;
-import View.UIBattleApp;
+import View.UI;
 import java.awt.Color;
 import java.awt.event.MouseEvent;
 import javax.swing.JButton;
 
-/**
- *
- * @author James-dt
- */
 public class DeploymentHandler implements PlayerDisplayListener {
     // Public fields
 
     public JButton bnAutoDeploy;
+    public JButton bnRotateShip;
 
     // Private fields
     private Cell[] currentCells = new Cell[0];
@@ -30,37 +22,38 @@ public class DeploymentHandler implements PlayerDisplayListener {
     private int currentX = -1;
     private int currentY = -1;
     private boolean isCurrentPositionValid = false;
+    private SingleTask myDisplayStatusTask = null;
     private GameWindow myGameWindow;
     private int nextShipIndex = 0;
+
+    // Accessors
+    public Ship getCurrentShip() {
+        Ship[] ships = this.getHumanShips();
+        if (this.nextShipIndex >= ships.length) {
+            return null;
+        } else {
+            return ships[this.nextShipIndex];
+        }
+    }
+
+    public Ship[] getHumanShips() {
+        return this.myGameWindow.getHumanPlayer().getShips();
+    }
+
+    public boolean isComplete() {
+        return this.nextShipIndex >= this.getHumanShips().length;
+    }
 
     // Constructors
     public DeploymentHandler(GameWindow gameWindow) {
         this.myGameWindow = gameWindow;
     }
 
-    // Accessors
-    public Ship getCurrentShip() {
-        Ship[] ships = getHumanShips();
-        if (nextShipIndex >= ships.length) {
-            return null;
-        } else {
-            return ships[nextShipIndex];
-        }
-    }
-
-    public Ship[] getHumanShips() {
-        return myGameWindow.getHumanPlayer().getShips();
-    }
-
-    public boolean isComplete() {
-        return nextShipIndex >= getHumanShips().length;
-    }
-
     // Public methods
     public void drawOverlay() {
-        Color drawColour = isCurrentPositionValid ? UIBattleApp.COLOUR_OCCUPIED : UIBattleApp.COLOUR_INVALID;
-        PlayerDisplay playerDisplay = myGameWindow.getHumanDisplay();
-        for (Cell coveredCell : currentCells) {
+        Color drawColour = this.isCurrentPositionValid ? UI.COLOUR_OCCUPIED : UI.COLOUR_INVALID;
+        PlayerDisplay playerDisplay = this.myGameWindow.getHumanDisplay();
+        for (Cell coveredCell : this.currentCells) {
             if (coveredCell == null) {
                 continue;
             }
@@ -69,114 +62,144 @@ public class DeploymentHandler implements PlayerDisplayListener {
     }
 
     public void onAutoDeploy() {
-        while (!isComplete()) {
-            autoDeployCurrentShip();
+        while (!this.isComplete()) {
+            this.autoDeployCurrentShip();
         }
-        complete();
+        this.complete();
     }
 
-    @Override
     public void onGridClicked(PlayerDisplay playerDisplay, MouseEvent e, int x, int y) {
-        onAutoDeploy();
+        if (e.getButton() == MouseEvent.BUTTON3) {
+            this.rotateShip();
+        } else {
+            this.setDeploymentPosition(x, y);
+            if (this.isCurrentPositionValid) {
+                this.deployCurrentShip();
+                this.redraw();
+
+                if (this.isComplete()) {
+                    this.complete();
+                }
+            } else {
+                final DeploymentHandler deploymentHandler = this;
+                myDisplayStatusTask = new SingleTask(1000) {
+                    public void runSingleTask() {
+                        deploymentHandler.displayCurrentStatus();
+                    }
+                };
+                PlayerDisplay humanDisplay = this.myGameWindow.getHumanDisplay();
+                humanDisplay.status(humanDisplay.getPlayer().getName() + ", you cannot deploy your " + this.getCurrentShip().getName() + " there.");
+            }
+        }
     }
 
-    @Override
     public void onGridEntered(PlayerDisplay playerDisplay, MouseEvent e, int x, int y) {
-        onAutoDeploy();
-        redraw();
+        this.setDeploymentPosition(x, y);
+        this.redraw();
     }
 
-    @Override
     public void onGridExited(PlayerDisplay playerDisplay, MouseEvent e, int x, int y) {
-        clearDeploymentPosition();
-        redraw();
+        this.clearDeploymentPosition();
+        this.redraw();
     }
 
     public void onSwitchOrientation() {
-        rotateShip();
+        this.rotateShip();
     }
 
     public void redraw() {
-        myGameWindow.getHumanDisplay().redraw();
-        drawOverlay();
+        this.myGameWindow.getHumanDisplay().redraw();
+        this.drawOverlay();
     }
 
     public void startGUI() {
-        deployComputerShips();
-        displayCurrentStatus();
+        this.deployComputerShips();
+        this.displayCurrentStatus();
 
-        bnAutoDeploy.setEnabled(true);
-        myGameWindow.getHumanDisplay().setEnabled(true);
-        myGameWindow.redraw();
+        this.bnAutoDeploy.setEnabled(true);
+       // this.bnRotateShip.setEnabled(true);
+        this.myGameWindow.getHumanDisplay().setEnabled(true);
+        this.myGameWindow.redraw();
     }
 
     // Private methods
     private void autoDeployCurrentShip() {
-        myGameWindow.getHumanPlayer().getGrid().autoDeploy(this.getCurrentShip());
-        ++nextShipIndex;
+        this.myGameWindow.getHumanPlayer().getGrid().autoDeploy(this.getCurrentShip());
+        ++this.nextShipIndex;
     }
 
     private void clearDeploymentPosition() {
-        setDeploymentPosition(-1, -1);
+        this.setDeploymentPosition(-1, -1);
     }
 
     private void complete() {
-        bnAutoDeploy.setEnabled(false);
-        myGameWindow.getHumanDisplay().setEnabled(false);
+        this.bnAutoDeploy.setEnabled(false);
+        this.bnRotateShip.setEnabled(false);
+        this.myGameWindow.getHumanDisplay().setEnabled(false);
 
-        nextShipIndex = getHumanShips().length;
-        clearDeploymentPosition();
-        redraw();
-        displayCurrentStatus();
+        this.nextShipIndex = this.getHumanShips().length;
+        this.clearDeploymentPosition();
+        this.redraw();
+        this.displayCurrentStatus();
 
-        myGameWindow.onDeployComplete(this);
+        this.myGameWindow.onDeployComplete(this);
     }
 
     private void deployComputerShips() {
-        Player computerPlayer = myGameWindow.getComputerPlayer();
+        Player computerPlayer = this.myGameWindow.getComputerPlayer();
         for (Ship ship : computerPlayer.getShips()) {
             computerPlayer.getGrid().autoDeploy(ship);
         }
-        myGameWindow.getComputerDisplay().status("The computer has deployed its ships.");
+        this.myGameWindow.getComputerDisplay().status("The computer has deployed its ships.");
+    }
+
+    private void deployCurrentShip() {
+        this.myGameWindow.getHumanPlayer().getGrid().deploy(this.getCurrentShip(), this.currentX, this.currentY, this.currentOrientation);
+        ++this.nextShipIndex;
+        this.revalidateCurrentPosition();
+        this.displayCurrentStatus();
     }
 
     private void displayCurrentStatus() {
-
-        Ship currentShip = getCurrentShip();
+        if (this.myDisplayStatusTask != null) {
+            this.myDisplayStatusTask.cancel();
+            this.myDisplayStatusTask = null;
+        }
+        Ship currentShip = this.getCurrentShip();
         if (currentShip == null) {
-            myGameWindow.getHumanDisplay().status("Deployment complete.");
+            this.myGameWindow.getHumanDisplay().status("Deployment complete.");
         } else {
-            myGameWindow.getHumanDisplay().status("Deploy your " + currentShip.getName() + ".");
+            this.myGameWindow.getHumanDisplay().status("Deploy your " + currentShip.getName() + ".");
         }
     }
 
     private void revalidateCurrentPosition() {
-        if (currentX == -1 || currentY == -1 || isComplete()) {
-            currentCells = new Cell[0];
-            isCurrentPositionValid = false;
+        if (this.currentX == -1 || this.currentY == -1 || this.isComplete()) {
+            this.currentCells = new Cell[0];
+            this.isCurrentPositionValid = false;
         } else {
-            Grid humanGrid = myGameWindow.getHumanPlayer().getGrid();
+            Grid humanGrid = this.myGameWindow.getHumanPlayer().getGrid();
             this.currentCells = humanGrid.coveredCells(
-                    this.getCurrentShip(), currentX, currentY, currentOrientation
+                    this.getCurrentShip(), this.currentX, this.currentY, this.currentOrientation
             );
-            isCurrentPositionValid = humanGrid.isValidDeployment(
-                    getCurrentShip(), currentX, currentY, currentOrientation
+            this.isCurrentPositionValid = humanGrid.isValidDeployment(
+                    this.getCurrentShip(), this.currentX, this.currentY, this.currentOrientation
             );
         }
     }
 
     private void rotateShip() {
-        currentOrientation = (currentOrientation == Ship.HORIZONTAL) ? Ship.VERTICAL : Ship.HORIZONTAL;
-        revalidateCurrentPosition();
-        redraw();
+        this.currentOrientation = (this.currentOrientation == Ship.HORIZONTAL) ? Ship.VERTICAL : Ship.HORIZONTAL;
+        this.revalidateCurrentPosition();
+        this.redraw();
     }
 
     private void setDeploymentPosition(int x, int y) {
-        if (currentX == x && currentY == y) {
+        if (this.currentX == x && this.currentY == y) {
             return;
         }
-        currentX = x;
-        currentY = y;
-        revalidateCurrentPosition();
+        this.currentX = x;
+        this.currentY = y;
+        this.revalidateCurrentPosition();
     }
 }
